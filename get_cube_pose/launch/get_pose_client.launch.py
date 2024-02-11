f70dc3977a32da5e82b947f1ae5c44af1a4ecb7c
Frank Hu <tianyaoh@hotmail.com>
2024-02-11 17:05:21 +0000

    get_cube_pose: +pointcloud_topic_arg

--- get_cube_pose/launch/get_pose_client.launch.py
+++ get_cube_pose/launch/get_pose_client.launch.py
@@ -2,20 +2,27 @@ import os
 from launch_ros.actions import Node
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, TextSubstitution
 from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     rviz_config_dir = os.path.join(get_package_share_directory('get_cube_pose'), 'rviz_config', 'launch_part.rviz')
 
-    print(f"TTTTTTTTTTTTTTTTTTT {get_package_share_directory('get_cube_pose'), 'rviz_config', 'launch_part.rviz'}")
+    #add launch argument for point cloud topic
+
 
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value="False", 
             description='Turn on/off sim time setting'
+        ),
+
+        DeclareLaunchArgument(
+            'pointcloud_topic',
+            default_value='/wrist_rgbd_depth_sensor/points', 
+            description='PointCloud2 topic for \'simple_grasp\' pkg to get info from.'
         ),
 
         Node(
@@ -31,7 +38,10 @@ def generate_launch_description():
             executable='basic_grasping_perception_node',
             name='basic_grasping_perception_node',
             output='screen',
-            parameters=[{'debug_topics': True}],
+            parameters=[
+                {'debug_topics': True},
+                {'pointcloud_topic': LaunchConfiguration('pointcloud_topic')},
+                ],
         ),
 
         Node(
@@ -39,6 +49,9 @@ def generate_launch_description():
             package='get_cube_pose',  
             executable='get_pose_client',  
             output='screen',
-            parameters=[{'use_sim_time': LaunchConfiguration("use_sim_time")}],
+            parameters=[
+                {'debug_topics': True},
+                {'pointcloud_topic': LaunchConfiguration('pointcloud_topic')},
+            ],
         ),
     ])
